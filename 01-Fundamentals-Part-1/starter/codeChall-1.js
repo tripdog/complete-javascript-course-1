3a26eb59bc40298ca486fa7b0f98c080b92a68f2
tripdog <eflips@yahoo.com>
2021-11-15 22:15:34 -0800

    keep on working

--- 01-Fundamentals-Part-1/starter/codeChall-1.js
+++ 01-Fundamentals-Part-1/starter/codeChall-1.js
@@ -8,9 +8,17 @@ let johnBMI = johnMass / johnHeight ** 2
 
 const higherBMI = markBMI > johnBMI
 
+//Code Challenge 1
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI is ${markBMI - johnBMI}lbs. higher than John's.`)
+}else{
+    console.log(`John's BMI is ${johnBMI - markBMI}lbs. higher than Mark's.`)
+}
+
+
 console.log(markBMI, johnBMI, higherBMI)
 
-//String interpolation
+//String interpolation////////////////////////////
 const firstName = 'Jonas'
 const job = 'teacher'
 const birthYear = 1991
@@ -30,7 +38,7 @@ console.log(`String with
 multiple
 lines`)
 
-//Conditionals-Control Structure
+//Conditionals-Control Structure///////////////////////
 const age = 13
 
 if(age >= 18){
@@ -41,3 +49,181 @@ if(age >= 18){
 }
 
 const continuationDate = 1968
+ let century
+ if (continuationDate <= 2000) {
+     century = 20
+ }else{
+     century = 21
+ }
+
+ console.log(century)
+
+ //Type Conversion  ////////////////////////////////////
+const inputYear = '1991'
+console.log(Number(inputYear), inputYear)
+console.log(Number(inputYear) + 18)
+
+console.log(Number('BigBopper'))
+console.log(typeof NaN)
+console.log(String(23))
+
+//Type Coercion ////////////////////////////////////////
+console.log('I am ' + 23 + ' years old')
+console.log('23' - '10' - 3)
+console.log('23' > '18')
+
+let n = '1' + 1
+n  = n - 1
+console.log(n)
+
+//Truthy and Falsey Values /////////////////////////////
+//Falsey Values are: 0, '', undefined, null, NaN
+
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean({}))
+console.log(Boolean('Tom'))
+
+const money = 0
+if (money) {
+    console.log("Don't spend it all.")
+}else{
+    console.log("You shouuld get a job.")
+}
+
+let height;
+if (height) {
+    console.log('Yay! Height is defined!')  
+}else{
+    console.log('Height is undefined :-(')
+}
+
+//Equality operators ////////////////////////////////////
+//Avoid the loose (==) equality operator if at all possible 
+let age = 18
+if (age === 18) {
+    console.log("You just became an adult.")
+}
+
+//When the user inputs this Number, it will become a String. 
+//So we wrap the prompt in the Number conversion function.
+const favNum = Number(prompt('Whats your favorite number'))
+
+if (favNum == 23) {
+    console.log("Cool, 23 is an amazing number.")
+} else if (favNum === 7) {
+    console.log("7 is also a cool number")
+}else{
+    console.log("The number is not 23 or 7.")
+}
+
+if (favNum !==23) {
+    console.log("Why not 23?")
+}
+
+//Logical Operators /////////////////////////////////////////////
+const hasDriverLic = true
+const hasGoodVision = true
+
+const shouldDrive = hasDriverLic && hasGoodVision
+
+if (hasDriverLic && hasGoodVision) {
+    console.log('Sarah is able to drive!')
+} else {
+    console.log('Someone else should drive')
+}
+
+const isTired = true
+console.log(hasDriverLic || hasGoodVision || isTired)
+
+
+console.log(!hasDriverLic)
+
+//Code Challenge #3 ////////////////////////////////////////////
+
+const dolphScore = (97 + 112 + 101) / 3
+const koalaScore = (109 + 95 + 106) / 3
+console.log(dolphScore)
+console.log(koalaScore)
+
+console.log(dolphScore, koalaScore)
+
+winner = dolphScore > koalaScore
+console.log(winner)
+
+if (dolphScore === koalaScore && dolphScore >= 100 && koalaScore >= 100) {
+    console.log('The match was a tie.')
+} else if (dolphScore > koalaScore && dolphScore >= 100) {
+    console.log("The Dolphin's win!")
+} else if(koalaScore > dolphScore && koalaScore >= 100){
+    console.log("The Koala's won!")
+} else {
+    "Nobody wins, insufficient points"
+}
+
+//The switch statement ///////////////////////////////////////////////
+const day = 'berzerker'
+
+switch(day){
+    case 'monday':
+        console.log("Plan course structure")
+        console.log("Go to coding meetup")
+        break;
+        case 'tuesday':
+            console.log("Prepare theory vids.")
+            break;
+        case 'wednesday':
+        case 'thursday':
+            console.log('Write code examples')
+            break;
+        case 'friday':
+            console.log('Record Vids')
+            break;
+        case 'saturday':
+        case 'sunday':
+            console.log('Enjoy the weekend')
+            break;
+        default:
+            console.log('You are not valid.')
+}
+//if else - else
+if (day === 'monday') {
+    console.log("Plan course structure.")
+    console.log("Go to coding meetup.")
+}else if (day === 'tuesday') {
+    console.log("Prepare theory vids.")
+}else if (day === 'wednesday' || day === 'thursday') {
+    console.log("Write code examples.")
+}else if (day === 'friday') {
+    console.log("Record Vids")
+}else if (day === 'saturday' || day === 'sunday') {
+    console.log("Enjoy the weekend.")
+}else{
+    console.log("You're not valid.")
+}
+
+//Statements(such as in if statement) and Expressions(3+4 produces a value therefor it is an expression)
+//Operators are expressions because they have values
+const age = 23
+age >= 18 ? console.log('You may drink booze.') : console.log('You may NOT booze it up!')
+
+//Conditionally assigning values to a variable (see below, POWERFUL!)
+const drink = age >= 18 ? 'Wine' : 'Water'
+console.log(drink)
+
+let drink2;
+if (age >=18) {
+    drink2 = 'wine'
+}else{
+    drink2 = 'water'
+}
+console.log(drink2)
+//Use the ternary operator inside a template literal for quick decisions
+console.log(`I like to drink ${age >= 18 ? 'Wine' : 'Water'}`)
+
+//Code challenge #4 tip calculator
+const bill = 430
+let tip;
+
+bill >= 50 && bill <= 300 ? tip = bill * .15 : tip = bill * .20;
+console.log(`The bill was ${bill} , the tip was ${tip} and the total was ${bill + tip} .`)
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,66 +1,257 @@
-'use strict' //makes it easier to introduce bugs
+'use strict' //makes it easier to find bugs and more difficult to introduce new bugs
 
-// const appleJuice = fruitProcessor(5, 0)
-// console.log(appleJuice)
-// console.log(fruitProcessor(5, 0))
-// const appleOrangeJuice = fruitProcessor(2, 4)
-// console.log(appleOrangeJuice)
+//The advantages of using strict mode //////////////////////////////
+// let hasDriversLicense = false
+// const passTest = true
+// if (passTest) hasDrveric = true
+// if (hasDriversLicense) console.log("Yes, I can drive.")
 
-// const num = Number("23")
-// console.log(num)
+//Functions /////////////////////////////////////////////////////
 
-// const calcAge1 = (birthYear) => {
-//     return 2021 - birthYear
-// }
-// const calcAge2 = (birthYear) => {
-//     return 2021 - birthYear
-// }
+//build and call a regular function
+function logger() {
+  console.log('My name is Tomas')
+}
+logger()
 
-// const calcAge3 = (birthYear) => 2021 - birthYear
-
-// const age1 = calcAge1(1978)
-// const age2 = calcAge2(1991)
-// const age3 = calcAge3(1986)
-
-// console.log(age1, age2, age3)
-
-const calcAge = function (birthYear) {
-    return 2021 - birthYear
+//calling / running / invoking functions 
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+  return juice
 }
 
-const yearsUntilRetirement = (birthYear, name) => {
-    const age = calcAge(birthYear)
-    const retirement = 65 - age
+//By running this function without saving it to a variable you just get 5 0. Only by assigning the result to a variable can you access the result of the function.
 
-    if (retirement > 0) {
-        console.log(`Hi ${name}, you have ${retirement} years until you can retire.`)
-        return retirement
-    } else {
-        console.log(`${name} has already retired`)
-        return -1
-    }
-    // return `Hi ${name}, you have ${retirement} years until you can retire.`
+//Even though this is assigned to a variable, the fn is still being called
+const appleJuice = fruitProcessor(5, 0) 
+console.log(appleJuice)
+
+//Same idea here assigning the value of the output to a variable and calling the fn
+const appleOrangeJuice = fruitProcessor(2, 4)
+
+//Keep Your Code DRY (Don't Repeat Yourself)
+
+//Function Declaration vs. Expressions ///////////////////////////////////////
+function calcAge1(birthYear) {
+  return 2037 - birthYear 
+}
+const age1 = calcAge1(1991)
+console.log(age1)
+
+//Function Expression (has no name aka anonymous function)
+const calcAge2 = function (birthYear){
+  return 2037 - birthYear 
+}
+age2 = calcAge2(1991)
+console.log(age2)
+
+//Arrow Functions //////////////////////////////////////////////////////////
+//Arrow functions are function expressions in short form//
+//Return is implicit and not necessary in the (one line) arrow function//
+const calcAge3 = birthYear => 2037 - birthYear
+const age3 = calcAge3(1991)
+console.log(age3)
+
+const yearsUntilRetirement = (birthYear,firstName) => {
+  const age = 2037 - birthYear
+  const retirement = 65 - age
+  return `${firstName} retires in ${retirement} years`
+}
+console.log(yearsUntilRetirement(1991, "Jonas")) 
+console.log(yearsUntilRetirement(1980, "Bob")) 
+
+//Arrow Functions don't work with the 'this' keyword.
+
+//Functions calling other functions ///////////////////////////////////////
+const cutPieces = function(fruit) {
+  return fruit * 4
 }
 
-console.log(yearsUntilRetirement(1945, "Tom"))
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutPieces(apples)
+  const orangePieces = cutPieces(oranges)
+  const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`
+  return juice
+}
+console.log(fruitProcessor(2, 3))
+
+const language = 'JavaScript'
+const str = language.toUpperCase();
+console.log(str) // output: ‘JavaScript’
+
+//Code Challenge /////////////////////////////////////////////////////////
+let calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
+
+let scoreDolphins = calcAverage(44, 23, 71)
+let scoreKoalas = calcAverage(65, 54, 49)
+
+// The above is okay BUT could be written better like this
+// ** const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
+// since the arrow function can be expressed in a single line.
+
+const avgDolphs = calcAverage(44, 23, 71)
+const avgKoalas = calcAverage(85, 54, 41)
+console.log(avgDolphs,avgKoalas)
+
+function checkWinner(avgDolphs, avgKoalas){
+if(avgDolphs >= 2 * avgKoalas){
+    console.log(`The Dolphins won! (${avgDolphs} vs ${avgKoalas})`)
+}else if(avgKoalas >= 2 * avgDolphs){
+  console.log(`The Koalas won! (${avgKoalas} vs. ${avgDolphs})`)
+} else {
+  console.log("There was no clear winner")
+}
+}
+
+checkWinner(scoreDolphins, scoreKoalas)
+checkWinner(576, 111)
+
+//Test data 2
+scoreDolphins = calcAverage(85, 54, 41)
+scoreKoalas = calcAverage(23, 34, 27)
+console.log(scoreDolphins, scoreKoalas)
+checkWinner(scoreDolphins, scoreKoalas)
 
 
-// const cutFruitPieces = function (fruit) {
-//     return fruit * 4
+//Arrays!
+const friend1 = 'Michael'
+const friend2 = 'Steven'
+const friend3 = 'Peter'
+
+//array literal
+const friends = ['Michael', 'Steven', 'Peter']
+console.log(friends[0])
+console.log(friends[2])
+//length property
+console.log(friends.length)
+console.log(friends[friends.length - 1])
+
+friends[2] = "Jay"
+
+//array constructor
+const years = new Array(1991, 1984, 2008, 2020)
+
+const firstName = "Jonas";
+const jonas = [firstName, 'Shmedtmann', 2037-1986, 'teacher', friends];
+console.log(jonas)
+
+//Exercise 
+const calculateAges = function (birthYear) {
+  return 2037 - birthYear
+}
+
+const years = [1990, 1967, 2002, 2010, 2018]
+calculateAges(years)
+
+const age1 = calculateAges(years[years.length -1])
+console.log(age1)
+
+const ages = [calculateAges(years[0]), calculateAges(years[1]), calculateAges(years[2])]
+
+console.log(ages)
+
+//add elements
+const friends = ['Michael', 'Steven', 'Peter']
+friends.push('Jay')
+
+const newFriends = friends.push('Tom')
+friends.unshift('John')
+console.log(friends)
+
+//Remove elements
+friends.pop()
+const popped = friends.pop()
+friends.unshift()
+console.log(popped)
+console.log(friends)
+
+
+
+//IndexOf && Includes
+console.log(friends.indexOf('Clark'))
+console.log(friends.includes('Bob'))
+console.log(friends.includes("Steven"))
+console.log(friends.includes(23))
+
+if(friends.includes('Steven')) {
+  console.log('YOu have a friend named Steven.')
+}
+
+////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+*/
+
+
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+} 
+const bills = [ 125, 555, 44]
+const tips = [ calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]) ]
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+
+console.log(bills, tips, totals)
+
+
+//OR USE AN ARROW FUNCTION
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * .15 : ball * .2 ;
+
+
+
+// const bill = 430;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// console.log(`The bill was ${bill}, and the tip was ${tip}, and the total value ${bill + tip}`)
+
+
+const jonasArray = [
+  'jonas',
+  'tom',
+  2037-1999,
+  'student',
+  ['ratan', 'bruno', 'Orion']
+];
+
+
+console.log(tom.friends[1])
+console.log(tom['lastName'])
+
+const tom = {
+  firstName: 'Tom',
+  lastName: 'Phillips',
+  age: 2037-1999,
+  job: 'developer',
+  friends: ['Bruno', 'George', 'Josh']
+}
+//This works because the key is the same as the last half of the variable name
+// we are building a string with the value of the key
+const nameKey = 'Name'
+
+// const interestedIn = prompt('What do you want to know about Tom? Choose between firstName, lastName, age, job and friends.')
+
+// if(tom) {
+//   console.log(tom[interestedIn])
+// } else {
+//   console.log('Wrong request')
 // }
 
-// const fruitProcessor = function (apples, oranges) {
-//     const applePieces = cutFruitPieces(apples)
-//     const orangePieces = cutFruitPieces(oranges)
-//     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`
-//     return juice
-// }
+tom.location = 'San Diego, CA, USA';
+console.log(tom)
 
-// console.log(fruitProcessor(2, 3))
-let total = 0;
-const calcAverage = (scores) => {
-  for (let i = 0; i < scores.length; i++) {
-    total += scores[i];
-  }
-  let avg = total / scores.length;
-};
+//Challenge #1
+//Tom has 3 friends, and his best friend is called Bruno.
+console.log(`${tom.firstName} has ${tom.friends.length} friends and his best friend is called ${tom.friends[0]}.`)
